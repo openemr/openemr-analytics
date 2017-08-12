0e9c58a4b493c778e8561d0fc12fdb08199dfca2
Jason 'Toolbox' Oettinger <jason@oettinger.email>
2017-08-11 22:55:44 -0400

    fixes for port number and preventing docker-compose re-download

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
+if [ ! -f docker-compose ]; then
+    curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+    chmod +x docker-compose
+fi
 ./docker-compose up -d --build
