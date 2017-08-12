069344835ced74e90fdda7e5df6909ddf4f920a1
Jason 'Toolbox' Oettinger <jason@oettinger.email>
2017-08-11 21:21:13 -0400

    fix to install-all

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
-docker-compose up -d --build
+./docker-compose up -d --build
